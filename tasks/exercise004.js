362d7155e71af7f7c97c2081e1b7e6e31321cc19
Osman Elsahib <elsahib10@gmail.com>
2020-10-28 16:32:52 +0000

    exercise004 done

--- tasks/exercise004.js
+++ tasks/exercise004.js
@@ -1,16 +1,38 @@
 // Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
 
+function isCharacterALetter(char) {
+    return (/[a-zA-Z]/).test(char)
+  }
+  
 function pigLatin(str){
     // You code here!
     let resultArr =[]
     let strArr = str.split(" ");
-    strArr.forEach(element => {
-        let temp = element.split("")
-        let first = temp.shift()
-        temp.push(first);
-        let word = temp.join("")
-        let result = word +"ay"
+
+    strArr.forEach((element) => {
+        let notLetter = []
+        let tempArr = element.split("")
+        tempArr.forEach((letter,i) =>{
+           if (!(/[a-zA-Z]/).test(letter)) {
+            notLetter.push(letter); 
+           }
+        })
+        if (notLetter.length > 0){
+            notLetter.forEach(letter =>{
+                tempArr.splice(tempArr.indexOf(letter),1) 
+            })
+        }
+
+        let first = tempArr.shift()
+        tempArr.push(first);
+        tempArr.push("ay")
+        notLetter.forEach(letter =>{
+            tempArr.push(letter)
+        })
+        
+        let result = tempArr.join("")
         resultArr.push(result)
+
     });
     
 
@@ -21,4 +43,4 @@ module.exports = {
     pigLatin
 };
 
-console.log(pigLatin('Pig latin is cool'));
+console.log(pigLatin('Pig latin is cool!!'));
